bfc7999f6d502145b71872626db624bda6b8e00c
sharmineelan <134765249+sharmineelan@users.noreply.github.com>
2023-05-29 17:51:45 +0530

    Add files via upload

---
+++ Kobi IT Hub 2.py
@@ -0,0 +1,16 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/ADMIN/AppData/Local/Programs/Python/Python311/Kobi IT Hub.py
+Hello, World!
+What is your name? kobika
+Hello  kobika
+>>> age=14
+>>> print(age)
+14
+>>> weight=40
+>>> print(weight)
+40
+>>> height=150
+>>> print(height)
+150
